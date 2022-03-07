74608a6771050bdafbfb606791a7fcdb1150a1be
chenyinheng30 <chenyinheng30@outlook.com>
2022-03-07 21:55:38 +0800

    为日志系统添加编译配置

--- include/chengine/log.h
+++ include/chengine/log.h
@@ -7,13 +7,6 @@
 #include"spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 
-#define CE_LEVEL_TRACE    spdlog::level::trace
-#define CE_LEVEL_DEBUG    spdlog::level::debug
-#define CE_LEVEL_INFO     spdlog::level::info
-#define CE_LEVEL_WARN     spdlog::level::warn
-#define CE_LEVEL_ERROR    spdlog::level::err
-#define CE_LEVEL_CRITICAL spdlog::level::critical
-#define CE_LEVEL_OFF      spdlog::level::off
 typedef spdlog::level::level_enum chengine_log_level;
 namespace chengine
 {
@@ -30,15 +23,9 @@ namespace chengine
         inline static std::shared_ptr<spdlog::logger>& get_core_logger(){return __core_logger;}
         inline static std::shared_ptr<spdlog::logger>& get_client_logger(){return __client_logger;}
         static void init();
-        static void set_stdout_level(chengine_log_level level);
-        static void set_your_pattern(const std::string& pattern);
-        static void set_your_name(const std::string& name);
     };
 }
 // Log Macros
-#ifndef __DEFAULT_LOG_FORMAT
-#define __DEFAULT_LOG_FORMAT "[%D %T.%e]%^<%l>%n%$-%t: %v"
-#endif
 #ifdef __CE_DEBUG
 #define CE_CORE_TRACE(...)  ::chengine::Log::get_core_logger()->trace(__VA_ARGS__)
 #define CE_CORE_INFO(...)   ::chengine::Log::get_core_logger()->info(__VA_ARGS__)
--- src/log.cpp
+++ src/log.cpp
@@ -1,17 +1,78 @@
 #include"chengine/log.h"
 
+#define __CE_LEVEL_TRACE    spdlog::level::trace
+#define __CE_LEVEL_DEBUG    spdlog::level::debug
+#define __CE_LEVEL_INFO     spdlog::level::info
+#define __CE_LEVEL_WARN     spdlog::level::warn
+#define __CE_LEVEL_ERROR    spdlog::level::err
+#define __CE_LEVEL_CRITICAL spdlog::level::critical
+#define __CE_LEVEL_OFF      spdlog::level::off
+
+/*default setting*/
+#define __CE_CORE_DEFAULT_LOG_LEVEL     __CE_LEVEL_TRACE
+#define __CE_CLIENT_DEFAULT_LOG_LEVEL   __CE_LEVEL_TRACE
+#define __DEFAULT_LOG_FORMAT            "[%D %T.%e]%^<%l>%n%$-%t: %v"
+#define __CE_DEFAULT_LOG_FILES_SIZE     5
+#define __CE_DEFAULT_LOG_FILES_COUNT    100
+#define __CE_DEFAULT_LOG_FILES_DIR      "logs/"
+#define __CE_DEFAULT_LOG_FILES_NAME     "chengine.log"
+
+#ifdef CE_CORE_USING_LOG_LEVEL
+#define CE_CORE_LOG_LEVEL CE_CORE_USING_LOG_LEVEL
+#else
+#define CE_CORE_LOG_LEVEL __CE_CORE_DEFAULT_LOG_LEVEL
+#endif
+
+#ifdef CE_CLIENT_USING_LOG_LEVEL
+#define CE_CLIENT_LOG_LEVEL CE_CLIENT_USING_LOG_LEVEL
+#else
+#define CE_CLIENT_LOG_LEVEL __CE_CLIENT_DEFAULT_LOG_LEVEL
+#endif
+
+#ifdef CE_CLIENT_USING_LOG_FORMAT 
+#define CE_CLIENT_LOG_FORMAT CE_CLIENT_LOG_FORMAT
+#else
+#define CE_CLIENT_LOG_FORMAT __DEFAULT_LOG_FORMAT
+#endif
+
+#ifdef CE_USING_LOG_FILES_SIZE
+#define CE_LOG_FILES_SIZE CE_USING_LOG_FILES_SIZE
+#else
+#define CE_LOG_FILES_SIZE __CE_DEFAULT_LOG_FILES_SIZE
+#endif
+
+#ifdef CE_USING_LOG_FILES_COUNT
+#define CE_LOG_FILES_COUNT CE_USING_LOG_FILES_COUNT
+#else
+#define CE_LOG_FILES_COUNT __CE_DEFAULT_LOG_FILES_COUNT
+#endif
+
+#ifdef CE_USING_LOG_FILES_DIR
+#define CE_LOG_FILES_DIR CE_USING_LOG_FILES_DIR
+#else
+#define CE_LOG_FILES_DIR __CE_DEFAULT_LOG_FILES_DIR
+#endif
+
+/*depend on CE_LOG_FILES_DIR*/
+#ifdef CE_USING_LOG_FILES_PATH
+#define CE_LOG_FILES_PATH CE_LOG_FILES_DIR CE_USING_LOG_FILES_NAME
+#else
+#define CE_LOG_FILES_PATH CE_LOG_FILES_DIR __CE_DEFAULT_LOG_FILES_NAME
+#endif
 namespace chengine
 {
 
     std::map<std::string,spdlog::sink_ptr> Log::__sinks;
     std::shared_ptr<spdlog::logger> Log::__core_logger;
     std::shared_ptr<spdlog::logger> Log::__client_logger;
-    void Log::init_sinks()
+    inline void Log::init_sinks()
     {
         __sinks["console"] = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        __sinks["files"] = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("logs/chengine.log", 1024*1024*5, 100);
+        const int log_files_size = CE_LOG_FILES_SIZE;
+        const int log_files_count = CE_LOG_FILES_COUNT;
+        __sinks["files"] = std::make_shared<spdlog::sinks::rotating_file_sink_mt>(CE_LOG_FILES_PATH,1024*1024*log_files_size,log_files_count);
     }
-    void Log::init_core_logger()
+    inline void Log::init_core_logger()
     {
 
 		__core_logger = std::make_shared<spdlog::logger>("CHENgine");
@@ -19,31 +80,23 @@ namespace chengine
         {
             __core_logger->sinks().push_back(sink.second);
         }
-		__core_logger->set_level(CE_LEVEL_TRACE);
+		__core_logger->set_level(CE_CORE_LOG_LEVEL);
         __core_logger->set_pattern(__DEFAULT_LOG_FORMAT);
     }
-    void Log::init_client_logger()
+    inline void Log::init_client_logger()
     {
 		__client_logger = std::make_shared<spdlog::logger>("your_app");
         for(auto& sink : __sinks)
         {
             __client_logger->sinks().push_back(sink.second);
         }
-		__client_logger->set_level(CE_LEVEL_TRACE);
-        __client_logger->set_pattern(__DEFAULT_LOG_FORMAT);
+		__client_logger->set_level(CE_CLIENT_LOG_LEVEL);
+        __client_logger->set_pattern(CE_CLIENT_LOG_FORMAT);
     }
     void Log::init()
     {
         Log::init_sinks();
         Log::init_core_logger();
         Log::init_client_logger();
-    }
-    void Log::set_stdout_level(chengine_log_level level)
-    {
-        __sinks["console"]->set_level(level);
-    }
-    void Log::set_your_pattern(const std::string &pattern)
-    {
-        __client_logger->set_pattern(pattern);
     }
 }
